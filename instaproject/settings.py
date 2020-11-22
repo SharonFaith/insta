5b51fd04a94da471f81e65f262f61153aedafca4
SharonFaith <sharonfaith15@gmail.com>
2020-11-22 17:06:15 +0300

    add email configuration

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -153,3 +153,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
