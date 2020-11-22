b1187a3a1bd4903849ba934abc2b5012e447e7b6
SharonFaith <sharonfaith15@gmail.com>
2020-11-22 19:44:08 +0300

    add image properties and link user model to profile model

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 import datetime as dt
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 
 class Profile(models.Model):
-#    profile_pic = models.ImageField(upload_to = 'uploads/', default = 'pics')
+    profile_pic = models.ImageField(upload_to = 'uploads/', default = 'pics')
     bio = models.TextField()
-#    user = link to user model foreign key
+    insta_user = models.ForeignKey(User, on_delete=models.CASCADE, default = None)
 
     def save_profile(self):
         self.save()
@@ -23,7 +24,7 @@ class Profile(models.Model):
         to_update.update(bio = updates)
 
 class Image(models.Model):
-#    image = models.ImageField(upload_to = 'uploads/', default = 'pics')
+    image = models.ImageField(upload_to = 'uploads/', default = 'pics')
     image_name = models.CharField(max_length=60)
     image_caption = models.TextField()
     profile_key = models.ForeignKey(Profile, on_delete=models.CASCADE)
