4610a41b025dc72cdfe8cff47314c4267142d185
SharonFaith <sharonfaith15@gmail.com>
2020-11-22 20:25:00 +0300

    test displaying model data

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse
-
+from .models import Image, Profile
 import datetime as dt
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
@@ -17,7 +20,10 @@ def welcome(request):
 
    return redirect(index)
 
+@login_required(login_url='/accounts/login')
 def index(request):
+   users = User.objects.all()
 
+   photos = Image.objects.all()
 
-    return render(request, 'index.html')
+   return render(request, 'index.html', {'photos': photos, 'users': users})
