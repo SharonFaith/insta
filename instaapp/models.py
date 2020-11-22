8c5d4fd6531bab02c105b8289a6a20029e1102d4
SharonFaith <sharonfaith15@gmail.com>
2020-11-22 17:38:28 +0300

    add image and profile models

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+
+
+
+class Profile(models.Model):
+#    profile_pic = models.ImageField(upload_to = 'uploads/', default = 'pics')
+     bio = models.TextField()
+#    uploader = maybe  to user model
+
+
+class Image(models.Model):
+#    image = models.ImageField(upload_to = 'uploads/', default = 'pics')
+    image_name = models.CharField(max_length=60)
+    image_caption = models.TextField()
+    profile_key = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    comments = models.TextField()
+
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image_name
+
+   
